e7d2442a8b0679d504a1c39f7e50cfd90737c93f
MiguelSerki <miguel.serki@gmail.com>
2017-11-16 09:16:27 -0300

    Corrected
    The calculator now properly ask you for a number and repites if you don't provide an int.

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,76 +10,76 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
-            string value;
             int x= 0;
             int y = 0;
             int z = 0;
             int w =  0;
+            int last = 0;
             Func<int, int, int> operacion = Resta;
             Func<int, int, int> operacion2 = Suma;
             Setter(ref x);
             Setter(ref y);
-            z = Op(x, y);
-            Setter(ref w);
-            int last = Op(z, w);
+            if (Op(x, y, ref z))
+            {
+                Setter(ref w);
+                Op(z, w, ref last);
+            }
+       
             Console.ReadLine();
 
         }
         private static void Setter (ref int x)
         {
-            string value;
-            int result = 0;
             string input;
-            Console.WriteLine("Ingrese un numero");
-            input = Console.ReadLine();
-            if (int.TryParse(input, out int int3))
+            do
             {
-                x = int.Parse(input);
+                Console.WriteLine("Ingrese un numero");
+                input = Console.ReadLine();
             }
-            else
-            {
-                x = 0;
-            }
+            while (!(int.TryParse(input, out int int3)));
+        x = int.Parse(input);
+
         }
-        private static int Op(int x, int y)
+        private static bool Op(int x, int y, ref int z)
         {
             string value;
-            int result = 0;
+            bool control = true;
             Console.WriteLine("Ingrese S para sumar, R para restar, M para multiplicar, o D para dividir");
             value = Console.ReadLine();
             switch (value.ToLower())
             {
                 case "s":
-                    result = Suma(x, y);
-                    Console.WriteLine("Tu resultado fue {0}", result);
+                    z = Suma(x, y);
+                    Console.WriteLine("Tu resultado fue {0}", z);
                     break;
                 case "r":
-                    result = Resta(x, y);
-                    Console.WriteLine("Tu resultado fue {0}", result);
+                    z = Resta(x, y);
+                    Console.WriteLine("Tu resultado fue {0}", z);
                     break;
                 case "m":
-                    result = Multi(x, y);
-                    Console.WriteLine("Tu resultado fue {0}", result);
+                    z = Multi(x, y);
+                    Console.WriteLine("Tu resultado fue {0}", z);
                     break;
                 case "d":
                     if (x == 0 || y == 0)
                     {
-                        Console.WriteLine("Estas dividendo por 0! Blasfemia!");
+                        Console.WriteLine("Estas dividiendo por 0! Blasfemia!");
+                        control = false;
                     }
                     else
                     {
-                        result = Divi(x, y);
-                        Console.WriteLine("Tu resultado fue {0}", result);
+                        z = Divi(x, y);
+                        Console.WriteLine("Tu resultado fue {0}", z);
                     }
                     break;
                 default:
                     Console.WriteLine("Has ingresado un caracter no valido");
-                    result = 0;
+                    control = false;
                     break;
             }
 
 
-            return result;
+            return control;
         }
         private static int Suma(int x, int y)
         {
